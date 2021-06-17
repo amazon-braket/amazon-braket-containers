61dc3da8eb19f72d5c69020bf6de4cd9330147b4
Milan Krneta <krneta@amazon.com>
2021-06-17 16:51:37 -0700

    Initial setup for the containers, based from DLC

---
+++ src/main.py
@@ -0,0 +1,34 @@
+"""
+Copyright 2021-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"). You
+may not use this file except in compliance with the License. A copy of
+the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+or in the "license" file accompanying this file. This file is
+distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ANY KIND, either express or implied. See the License for the specific
+language governing permissions and limitations under the License.
+"""
+
+import argparse
+
+import utils
+from image_builder import image_builder
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Program to build docker images")
+    parser.add_argument("--buildspec", type=str)
+    parser.add_argument("--framework", type=str)
+    args = parser.parse_args()
+
+    utils.build_setup(args.framework)
+
+    image_builder(args.buildspec)
+
+
+if __name__ == "__main__":
+    main()
