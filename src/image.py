38f8bcac50214ec1aabc9927b4f246d7e86f1b98
Abe Coull <accoull@amazon.com>
2023-09-20 16:14:13 -0700

    remove extra stuff..

--- src/image.py
+++ src/image.py
@@ -131,48 +131,9 @@ class DockerImage:
                 cache_from = [f"{self.repository}:{self.cache_tag}"]
                 response.append(f"Setting cache to: {cache_from}")
 
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print(self.dockerfile)
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-            print("/n")
-
-
             for line in self.client.build(
                 fileobj=context_file,
-                path="base/jobs/docker/1.0/py3/Dockerfile.cpu",
+                path="self.dockerfile",
                 custom_context=True,
                 rm=True,
                 decode=True,
