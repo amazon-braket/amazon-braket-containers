745fec42ec5e3dcc24780091f1fabbcacfe42fc6
Gandhi <gandhiramu@users.noreply.github.com>
2025-08-14 06:42:30 -0700

    feat: add CUDA-Q container framework support (#363)
    Co-authored-by: Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>
    Co-authored-by: Viraj Chaudhari <72896239+virajvchaudhari@users.noreply.github.com>

--- test/resources/qaoa_utils.py
+++ test/resources/qaoa_utils.py
@@ -26,6 +26,16 @@ class QAOAInterface(ABC):
     def get_cost_and_step(cls, cost_function, params, optimizer):
         pass
 
+    @classmethod
+    def supports_full_optimization(cls):
+        """Return True if this interface handles full optimization internally"""
+        return False
+    
+    @classmethod
+    def run_full_optimization(cls, cost_function, initial_params, optimizer, num_iterations):
+        """Run complete optimization (only for interfaces that support it)"""
+        raise NotImplementedError("This interface doesn't support full optimization")
+
     @staticmethod
     # Get the appropriate interface to use based on the string input.
     def get_interface(interface):
@@ -35,6 +45,8 @@ class QAOAInterface(ABC):
             return TensorFlowInterface()
         elif interface == "torch":
             return PyTorchInterface()
+        elif interface == "cudaq":
+            return CudaQInterface()
         else:
             raise ValueError(f"Interface {interface} is invalid.")
 
@@ -125,3 +137,62 @@ class PyTorchInterface(QAOAInterface):
         cost_before.backward()
         optimizer.step()
         return params, float(cost_before)
+
+
+class CudaQInterface(QAOAInterface):
+    def __init__(self):
+        global cudaq, np
+        cudaq = __import__("cudaq", globals(), locals())
+        np = __import__("numpy", globals(), locals())
+
+    @classmethod
+    def initialize_params(cls, np_array):
+        return np_array
+
+    @classmethod
+    def supports_full_optimization(cls):
+        return True
+
+    @classmethod
+    def get_sgd_optimizer(cls, stepsize, params):
+        optimizer = cudaq.optimizers.Adam()
+        gradient = cudaq.gradients.CentralDifference()
+        return {"optimizer": optimizer, "gradient": gradient}
+
+    @classmethod
+    def convert_params_to_numpy(cls, params):
+        return np.array(params)
+
+    @classmethod
+    def get_cost_and_step(cls, cost_function, params, optimizer):
+        # Not used for CUDA-Q (uses full optimization instead)
+        cost_before = cost_function(params)
+        return params, float(cost_before)
+    
+    @classmethod
+    def run_full_optimization(cls, cost_function, initial_params, optimizer_config, num_iterations):
+        optimizer = optimizer_config["optimizer"]
+        gradient = optimizer_config["gradient"]
+        
+        def objective_function(parameter_vector):
+            params_shaped = np.array(parameter_vector).reshape(initial_params.shape)
+            cost = cost_function(params_shaped)
+            
+            def cost_func_flat(x):
+                return cost_function(np.array(x).reshape(initial_params.shape))
+            
+            gradient_vector = gradient.compute(parameter_vector, cost_func_flat, cost)
+            return cost, gradient_vector
+        
+        flat_params = initial_params.flatten()
+        final_cost, final_params = optimizer.optimize(dimensions=len(flat_params), 
+                                                     function=objective_function)
+        
+        return np.array(final_params).reshape(initial_params.shape), final_cost
+    
+    @classmethod
+    def get_pennylane_interface(cls):
+        return "autograd"
+
+
+
